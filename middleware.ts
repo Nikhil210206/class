d3bf817e2184dbf2fd8bfb68cbfaa2adf087103a
Nikhil <nikhilbalamurugan@gmail.com>
2025-06-06 12:44:51 +0530

    middle

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const cookie = (await cookies()).get("token")?.value;
+  if (req.nextUrl.pathname === "/auth/login" && cookie) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/app/timetable";
+    return NextResponse.redirect(url);
+  }
+  if (req.nextUrl.pathname !== "/auth/login" && !cookie) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/auth/login";
+    return NextResponse.redirect(url);
+  }
+
+  if (req.nextUrl.pathname === "/app" && cookie) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/app/timetable";
+    return NextResponse.redirect(url);
+  }
+  if (req.nextUrl.pathname === "/app" && !cookie) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/auth/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|Manifest|favicon.ico|api/|.*\\.(?:txt|xml|js|svg|png|jpg|jpeg|gif|webp)$|$).*)",
+  ],
+};
